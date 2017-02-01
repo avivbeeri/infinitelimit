c86502a38bbb148fa00aba240a54406578433c63
Aviv Beeri <avbeeri@gmail.com>
2017-02-01 22:13:43 +0000

    Wired up express to prismic and started the skeleton stuff

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const Prismic = require('prismic.io');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+function resolve(doc) {
+  if (doc.type !== 'article') {
+    return '/' + doc.uid;
+  }
+  return '/article/' + doc.uid;
+}
+
+function api() {
+  return Prismic.api('https://infinitelimit.prismic.io/api', {
+    apiDataTTL: 60
+  });
+}
+
+app.get('/', (req, res, next) => {
+  // Retrieve the home page, render and serve
+  res.status(200).send('Welcome to infinitelimit.net');
+});
+
+app.get('/favicon.ico', (req, res) => {
+  res.status(204).send();
+});
+
+app.get('/article/:uid', (req, res, next) => {
+  api().then((api) => {
+    return api.getByUID('article', req.params.uid);
+  })
+  .then((doc) => {
+    const locals = {
+      title: doc.getStructuredText('article.title').asText(),
+      image: doc.getImage('article.thumbnail').url
+    };
+    res.send(locals);
+  })
+  .catch((reason) => {
+    next(reason);
+  }); 
 });
 
 app.listen(process.env.PORT || 3000, () => {
